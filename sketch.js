d82c04c1855faf1877c0153813ea08e810c1911c
Narendra <parandku@gmail.com>
2020-12-09 12:59:26 +0530

    classs29

--- sketch.js
+++ sketch.js
@@ -36,14 +36,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:100});
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
     background(backgroundImg);
+    textSize (30);
+    text ("angry birds game by Narendra",360,30);
     Engine.update(engine);
     strokeWeight(4);
     box1.display();
@@ -75,3 +77,4 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
